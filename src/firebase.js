15bde38cbcd85c918b6d0297b14f9d8a88be0715
Alejandro Obregon <aobregonrodri@gmail.com>
2022-04-03 12:16:42 +0200

    git ignore

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { initializeApp } = require("firebase-admin/app");
 const admin = require("firebase-admin");
 const { getFirestore } = require("firebase-admin/firestore");
-const serviceAccount = require("../claveSDK/fluttergewete-firebase-adminsdk-syr4r-02a2f56b6f.json");
+const serviceAccount = require("../claveSDK/adminsdk.json");
 
 initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const db = getFirestore();
-module.exports = { db };
+module.exports = { db }; 
